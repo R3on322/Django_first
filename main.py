0b086f0bc1e57bbd626b35548f634d8cd101ba73
R3on322 <kindeer123@mail.ru>
2022-06-06 19:25:21 +0300

    [upd] Рабочая версия, с работающим верно POST запросом.

--- main.py
+++ main.py
@@ -4,11 +4,10 @@ from rbr_srv_side import DESC
 
 
 def POST_zapros():
-    desc_inf = str(DESC.PC_info.main_inf())
     new_data = {
                 'name': 'hostname',
                 'ip_address': requests.get('https://ipv4-internet.yandex.net/api/v0/ip').text.strip('"'),
-                'description': desc_inf
+                'description': str(DESC.PC_info.main_inf())
                 }
     url = 'http://127.0.0.1:8000/api/servers/add_post'
     response = requests.post(url, json=new_data)
